db5e939ddba4a161c3f5c27c8fdf44aac4c983f6
sudharsan <sudharsan.sivanantham@colanonline.com>
2024-02-12 17:56:51 +0530

    replica design fix

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 function Links() {
   return (
     <ul className='header-nav'>
-        <li><Link to={'/'}>Home</Link></li>
-        <li><Link to={'/about'}>About</Link></li>
+        {/* <li><Link to={'/'}>Home</Link></li> */}
+        {/* <li><Link to={'/about'}>About</Link></li> */}
         {/* <li><Link to={'/todo'}>To-Do</Link></li> */}
         <li><Link to={'/todo-list'}>To-Do-List</Link></li>
         <li><Link to={'/replica'}>Replica</Link></li>
